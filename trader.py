1053d1d070b5335bbbf8821835f17cd5ceb2e96a
Tim Pilien <ubuntu@bot-jurien.eth1-sydney.js-eth>
2016-03-18 23:39:54 +0000

    fix hash

--- trader.py
+++ trader.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python
+`x#!/usr/bin/python
 from __future__ import print_function
 
 import sys
 import socket
+import json
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("10.0.254.41", 20000))
+    s.connect(("10.0.254.41", 25002))
     return s.makefile('w+', 1)
 
 def main():
     exchange = connect()
-    print("HELLO jurien", file=exchange)
-    hello_from_exchange = exchange.readline().strip()
+    print("HELLO JURIEN", file=exchange)
+    hello_from_exchange = json.loads(exchange.readline())
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
 
 if __name__ == "__main__":
