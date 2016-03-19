383c06277faec503c23f35436eecda6ef453da45
Nevin <nevin.lazarus@gmail.com>
2016-03-19 12:04:46 +1100

    added money global

--- trader.py
+++ trader.py
@@ -5,6 +5,12 @@ import sys
 import socket
 import json
 
+#GLOBALS
+
+money = 0
+bond_fair = 1000
+
+
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("10.0.254.41", 25002))
@@ -15,13 +21,50 @@ def add(order_id, symbol, direction, price, size):
     #direction is buying / selling
 
     json_string = '{"type": "add", "order_id": "' + str(order_id) + '", "symbol": "' + symbol + '", "dir": "' + direction + '", "price": "' + str(price) + '", "size": "'+ str(size) +'"}'
+    return json_string
+    
     
 def convert(order_id, symbol, direction, price, size):
     json_string = '{"type": "convert", "order_id": "' + str(order_id) + '", "symbol": "' + symbol + '", "dir": "' + direction + '", "size": "'+size +'"}'
+    return json_string
     
 def cancel(order_id):
     json_string = '{"type": "cancel", "order_id": "'+ str(order_id) + '"}'
+    return json_string
     
+    
+def processServerResponse(json_response):
+    response_dict = json.loads(json_response)
+    response_type = response_dict["type"]
+    global money
+    if response_type == "hello":
+        money = response_dict["cash"]
+    elif response_type == "open":
+        #update list of open orders
+        pass
+    elif response_type == "close":
+        pass
+    elif response_type == "error":
+        print(response_dict["error"])
+    elif response_type == "book":
+        #update our local copy of the book
+        pass
+    elif response_type == "trade":
+        pass
+    elif response_type == "ack":
+        #our order went through
+        pass
+
+    elif response_type == "reject":
+        #remove the order from out local list
+        print response_dict["order_id"], response_dict["error"]
+
+    elif response_type == "fill":        
+        pass
+    elif response_type == "out":    
+        pass
+        
+    return response_dict
     
 
 
