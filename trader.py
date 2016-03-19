66a67cc9904905b080c22bb4b5041df35f44faab
Tim Pilien <ubuntu@bot-jurien.eth1-sydney.js-eth>
2016-03-19 00:31:17 +0000

    test

--- trader.py
+++ trader.py
@@ -7,7 +7,7 @@ import json
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("10.0.254.41", 25001))
+    s.connect(("10.0.254.41", 25000))
     return s.makefile('w+', 1)
 
 def main():
@@ -15,7 +15,21 @@ def main():
     json_string = '{"type": "hello", "team": "JURIEN"}'
     print(json_string, file=exchange)
     hello_from_exchange = json.loads(exchange.readline())
-    print("The exchange replied:", hello_from_exchange["cash"], file=sys.stderr)
+
+
+    json_string = '{"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 999 "size": 1}'
+    print(json_string, file=exchange)
+    while 1:
+      hello_from_exchange = json.loads(exchange.readline())
+      json_string = '{"type": "add", "order_id": 1, "symbol": "BOND", "dir": "SELL", "price": 1001 "size": 1}'      
+      try:
+        print(json_string, file=exchange)
+      except:
+        pass
+      try:
+        hello_from_exchange = json.loads(exchange.readline())
+      except:
+        pass
 
 if __name__ == "__main__":
     main()
